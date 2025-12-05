3af4d3e38f009dafcfe580b71cef81852d7838e9
Pranay Gopidi <153822005+pranay-018@users.noreply.github.com>
2025-12-05 08:04:47 +0530

    Update App.java

--- dec4/App.java
+++ dec4/App.java
@@ -1,18 +1,18 @@
-package com.java.tax_calculation;
+package com.project.tax_calculation;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import com.java.entites.Login;
-import com.java.entites.Property;
-import com.java.entites.Vechile;
-import com.java.enums.FuelType;
-import com.java.exceptions.InValidCredentials;
-import com.java.exceptions.InValidInputException;
-import com.java.services.PropertyService;
-import com.java.services.VechileService;
+import com.project.entities.Login;
+import com.project.entities.Property;
+import com.project.entities.Vehicle;
+import com.project.enums.FuelType;
+import com.project.exceptions.InValidCredentials;
+import com.project.exceptions.InValidInputException;
+import com.project.services.PropertyService;
+import com.project.services.VechileService;
 
 //*************************TAX CALCULATION APPLICATION********************************* 
 public class App {
@@ -24,11 +24,9 @@ public class App {
 		System.out.println("PLEASE LOGIN TO CONTINUE  -");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the username : ");
-//		String userName = sc.nextLine();
-		String userName = "pranay";
+		String userName = sc.nextLine();
 		System.out.println("Enter the password  : ");
-//		String password = sc.nextLine();
-		String password = "pranay@18";
+		String password = sc.nextLine();
 		try {
 			Login.validateCredentials(userName, password);
 		} catch (InValidCredentials e) {
@@ -37,7 +35,7 @@ public class App {
 
 		}
 		List<Property> properties = new ArrayList<Property>();
-		List<Vechile> vechiles = new ArrayList<Vechile>();
+		List<Vehicle> vehicles = new ArrayList<Vehicle>();
 		int option = 4;
 		do {
 			Menu.displayOperationsMenu();
@@ -48,10 +46,10 @@ public class App {
 					PropertyService.propertyDetails(properties);
 					break;
 				case 2:
-					VechileService.vechileDetails(vechiles);
+					VechileService.vechileDetails(vehicles);
 					break;
 				case 3:
-					getTotalTax(vechiles, properties);
+					getTotalTax(vehicles, properties);
 					break;
 				case 4:
 					break;
@@ -60,7 +58,7 @@ public class App {
 				}
 
 			} catch (InputMismatchException Exception) {
-				System.out.println("Invalid operation...please select appropriate operation");
+				System.out.println("Invalid operation...please enter the valid format data");
 				sc.nextLine();
 				continue;
 			}
@@ -68,17 +66,33 @@ public class App {
 		System.out.println("closing Application.....closed");
 	}
 
-	public static void getTotalTax(List<Vechile> vechiles, List<Property> properties) {
-		double vechiletax = 0.0;
-		double propertytax = 0.0;
-		for (Vechile vechile : vechiles) {
-			vechiletax += vechile.getTax();
-		}
-		for (Property property : properties) {
-			propertytax += property.getTax();
+	public static void getTotalTax(List<Vehicle> vehicles, List<Property> properties) {
+		double vehicleTax = 0.0;
+		double propertyTax = 0.0;
+
+		for (Vehicle vehicle : vehicles) {
+			vehicleTax += vehicle.getTax();
 		}
 
-		System.out.println("vechiles total tax : " + vechiletax);
-		System.out.println("properties total tax : " + propertytax);
+		for (Property property : properties) {
+			propertyTax += property.getTax();
+		}
+
+		double totalTax = vehicleTax + propertyTax;
+		System.out.println("+-----------------------------------------------------------------------+");
+		System.out.printf("| %-6s | %-20s | %-10s | %-15s |\n", "SR. NO.", "PARTICULAR", "QUANTITY", "TAX");
+		System.out.println("+-----------------------------------------------------------------------+");
+
+		System.out.printf("| %-6d | %-20s | %-10d | %-15.2f |\n", 1, "PROPERTIES", properties.size(), propertyTax);
+
+		System.out.printf("| %-6d | %-20s | %-10d | %-15.2f |\n", 2, "VEHICLES", vehicles.size(), vehicleTax);
+
+		System.out.println("+-----------------------------------------------------------------------+");
+
+		System.out.printf("| %-6s | %-20s | %-10d | %-15.2f |\n", "TOTAL", "--------",
+				(properties.size() + vehicles.size()), totalTax);
+
+		System.out.println("+-----------------------------------------------------------------------+");
+
 	}
 }
